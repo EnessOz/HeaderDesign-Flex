99351954b0ee84e2e26c19376bfeff057ce408f6
Enes Ozturk <enes55ozturk@gmail.com>
2023-02-27 15:16:49 +0300

    CSS-Flex

---
+++ java.js
@@ -0,0 +1,21 @@
+function home() {
+    document.getElementById("home").innerHTML = "Clicked Home";
+}
+function about() {
+    document.getElementById("about").innerHTML = "Clicked About";
+}
+function conctract() {
+    document.getElementById("conctract").innerHTML = "Clicked Contract";
+}
+function gallery() {
+    document.getElementById("gallery").innerHTML = "Clicked Gallery";
+}
+function register() {
+    document.getElementById("register").innerHTML = "Clicked Register";
+}
+
+function signin() {
+    document.getElementById("signin").innerHTML = "Clicked Sign in";
+}
+
+
